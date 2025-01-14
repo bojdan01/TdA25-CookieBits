8fa77459144f7ff67111783addcd6f7eb113eafd
Matycz07 <83537224+Matycz07@users.noreply.github.com>
2025-01-14 14:12:16 +0100

    xo

--- public/app/piskvorky-c.js
+++ public/app/piskvorky-c.js
@@ -1,6 +1,18 @@
 const HraciPole = document.querySelector('#game-container');
+let gameState = 'opening';
+let Turn = true;
 
 HraciPole.addEventListener('click', (e) => {
-    console.log(e.target.className);
-    e.target.className = "game-x";
+
+    if (e.target.className == '') {
+        if (Turn == true) {
+            e.target.className = "game-x";
+            Turn = false;
+        } else {
+            e.target.className = "game-o";
+            Turn = true;
+        }
+
+    }
+
 });
